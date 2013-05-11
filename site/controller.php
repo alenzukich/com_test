d531c2f8c99e1ca3cdd94fbf7b8cbeefd648c961
alenzukich <alen@camcloud.com>
2013-05-10 21:07:48 -0400

    -controller code added

--- site/controller.php
+++ site/controller.php
@@ -14,5 +14,36 @@ jimport('joomla.application.component.controller');
 
 class TestController extends JController
 {
-
+	function __construct() {
+		// params
+		$jinput = JFactory::getApplication()->input;
+		$this->username = $jinput->get('username', '', 'STRING');
+		$this->password = $jinput->get('password', '', 'STRING');
+		$this->checkRequiredParameters();
+	}
+	
+	private function checkRequiredParameters() {
+		if (($this->username == '') ||	($this->password == '')) 
+		{
+			header('HTTP/1.1 400 Bad Request', true, 400);
+			jexit();
+		}
+	}
+	
+	function execute() {
+		jimport( 'joomla.user.authentication');
+		$auth = & JAuthentication::getInstance();
+		$credentials = array( 'username' => $this->username, 'password' => $this->password );
+		$options = array();
+		$response = $auth->authenticate($credentials, $options);
+	
+		if ($response->status != JAUTHENTICATE_STATUS_SUCCESS)
+		{
+			echo "Oh Snap! Failure!";
+		}
+		else
+		{
+			echo "W00t! Success";
+		}
+	}
 }
